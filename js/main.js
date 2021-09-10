bed09f97b0683dc831eb4d20f7651a10f835da54
hai0bd <88884589+hai0bd@users.noreply.github.com>
2021-09-10 21:53:47 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ function firstQuestion() {
               center bottom
               no-repeat
             `,
-            html: "<video width='300px' controls autoplay> <source src = 'video/IMG_E3362.MOV ' <source src='video/IMG_E3362.ogv' /> <source src='video/IMG_E3362.webm' /> > </video> ",
+            html: "<video width='300px' controls autoplay> <source src = 'video/IMG_E3362.MOV ' /> > </video> ",
             background: '#fff url("img/birthday.jpg")',
             imageAlt: 'Custom image',
             confirmButtonText: CONFIG.btnIntro
